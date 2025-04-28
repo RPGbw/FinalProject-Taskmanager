e65d9e32e0b90fe3435876fa45b922896be9ef9a
RPGbw <brennan.whiting2002@gmail.com>
2025-04-28 09:23:10 -0600

    WhitingBrennanFinalProject.py

--- WhitingBrennanFinalProject.py
+++ WhitingBrennanFinalProject.py
@@ -1,5 +1,11 @@
+'''
+Author:Brennan Whiting
+date:04/27/2025
+Program: To Do list manager
+'''
+
 from breezypythongui import EasyFrame
-import tkinter as tk  # Needed for tk.END constant
+import tkinter as tk
 
 class TodoApp(EasyFrame):
     def __init__(self):
@@ -8,9 +14,9 @@ class TodoApp(EasyFrame):
         # Add components
         self.addLabel("Enter Task:", row=0, column=0)
         self.taskField = self.addTextField(text="", row=0, column=1)
-        self.addButton = self.addButton("Add", row=0, column=2, command=self.add_task)
-        self.taskList = self.addListBox(row=1, column=0, columnspan=3)  # Note: ListBox with capital B
-        self.clearButton = self.addButton("Clear All", row=2, column=0, columnspan=3, command=self.clear_tasks)
+        self.add_button = self.addButton("Add", row=0, column=2, command=self.add_task)
+        self.taskList = self.addListbox(row=1, column=0, columnspan=3)
+        self.clear_button = self.addButton("Clear All", row=2, column=0, columnspan=3, command=self.clear_tasks)
         
         # Bind click event
         self.taskList.bind("<Button-1>", self.toggle_task)
@@ -24,20 +30,17 @@ class TodoApp(EasyFrame):
             self.messageBox("Warning", "Please enter a task.")
     
     def toggle_task(self, event):
-        # Get the clicked item index
         index = self.taskList.curselection()
-        if index:  # If an item is selected
+        if index:
             index = index[0]
-            # Get current state
             current_fg = self.taskList.itemcget(index, "fg")
-            # Toggle state
             if current_fg == "black":
                 self.taskList.itemconfig(index, fg="gray", overstrike=1)
             else:
                 self.taskList.itemconfig(index, fg="black", overstrike=0)
     
     def clear_tasks(self):
-        if self.questionBox("Confirm", "Clear all tasks?"):
+        if self.taskField("Confirm", "Clear all tasks?"):
             self.taskList.delete(0, tk.END)
 
 if __name__ == "__main__":
